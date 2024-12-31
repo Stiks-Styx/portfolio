3583a452c799093bced99862746bca6bc44e7b93
Stiks-Styx <aa1305589@gmail.com>
2024-12-31 15:13:19 +0800

    final real

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ import Grid from "@/components/Grid";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
       <FloatingNav navItems={navItems} />
         <Hero />
